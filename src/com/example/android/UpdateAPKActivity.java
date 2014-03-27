c065716409fc57054ae47b7775c29ffccfe08e5d
Zhangss <song_88@163.com>
2014-03-27 13:22:26 +0800

    版本升级修改提交

--- src/com/example/android/UpdateAPKActivity.java
+++ src/com/example/android/UpdateAPKActivity.java
@@ -5,9 +5,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -36,7 +42,11 @@ public class UpdateAPKActivity extends Activity {
 			switch (msg.what) {
 			case 0x123: {
 				Log.v("Update", "reveiveMessage");
-				installAPK(apkFile);
+				if (apkFile != null){
+					installAPK(apkFile);
+				}else {
+					Log.e("Update", "下载失败,File 不存在!");
+				}
 				break;
 			}
 			default:
@@ -60,8 +70,9 @@ public class UpdateAPKActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				// 百度贴吧测试url
-				final String urlString = "http://gdown.baidu.com/data/wisegame/6eb90709a4e436f2/baidutieba_80.apk";
-
+				
+//				final String urlString = "http://gdown.baidu.com/data/wisegame/6eb90709a4e436f2/baidutieba_80.apk";
+				final String urlString = "http://10.32.17.125:8080/BarTackApp.apk";
 				new Thread(new Runnable() {
 
 					@Override
@@ -77,49 +88,53 @@ public class UpdateAPKActivity extends Activity {
 	}
 
 	// 解析XML内容
-	@SuppressWarnings("unused")
 	private File downloadAPK(String urlString) {
 		/**
-		 * Andorid文件路径问题
+		 * Andorid文件路径问题 优先创建SD卡目录
 		 */
-		String path = null;
-		if (Environment.getExternalStorageState().equals(
-				Environment.MEDIA_MOUNTED) && false) {
-			path = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-		} else {
+		String path = createFileInSDCard("update");
+		if (path == null) {
 			path = createFileInAppData("update");
-			Log.e("Update",
-					"SDCard State:" + Environment.getExternalStorageState());
 		}
 		File file = new File(path, "update.apk");
-//		File file = new File(path + "/update.apk");
-//		createFileInSDCard("AndroidTest");
 		if (file.exists()) {
 			Log.e("Update", "Update is Exist!");
-			return file;
+			file.delete();
+			// return file;
 		}
 		Log.e("zhangss", "Download URL:" + urlString);
 		URL url;
 		try {
 			url = new URL(urlString);
-
+			long timeStart = System.currentTimeMillis();
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			String fileName = getFileName(conn);
 			InputStream is = conn.getInputStream();
 			FileOutputStream fos = new FileOutputStream(file);
 			BufferedInputStream bis = new BufferedInputStream(is);
-			// 分配内存过多会直接崩溃 byte 分配1k空间
-			byte[] buffer = new byte[1024];
+			int total = conn.getContentLength();
+			int progress = 0;
+			// 分配内存过多会直接崩溃 byte 分配1k空间为1024
+			// 减少分配的空间 可以降低下载文件内容不完全的问题
+			byte[] buffer = new byte[64];
 			int len;
 			int count = 0;
 			while ((len = bis.read(buffer)) != -1) {
 				fos.write(buffer, 0, len);
 				count++;
+				progress += len;
 				Log.v("Update", "Progress" + count);
 			}
-			fos.close();
+			if (progress != total) {
+				Log.v("Update", "DownLoad Incomplete");
+			}
+			Log.v("Update", progress + "/" + total);
+			Log.v("Update", "DownLoad Time:"
+					+ (System.currentTimeMillis() - timeStart) + "");
 			bis.close();
+			fos.close();
 			is.close();
+			conn.disconnect();
 			return file;
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -131,6 +146,54 @@ public class UpdateAPKActivity extends Activity {
 
 		}
 		return null;
+	}
+
+	/**
+	 * 获取下载的文件名称
+	 * @param conn
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	private String getFileName(HttpURLConnection conn) {
+		String filename = "";
+		boolean isok = false;
+		// 从UrlConnection中获取文件名称
+		Map<String, List<String>> hf = conn.getHeaderFields();
+		if (hf == null) {
+			return null;
+		}
+		Set<String> key = hf.keySet();
+		if (key == null) {
+			return null;
+		}
+
+		for (String skey : key) {
+			List<String> values = hf.get(skey);
+			for (String value : values) {
+				String result;
+				try {
+					result = new String(value.getBytes("ISO-8859-1"), "GBK");
+					int location = result.indexOf("filename");
+					if (location >= 0) {
+						result = result.substring(location + "filename".length());
+						filename = result.substring(result.indexOf("=") + 1);
+						isok = true;
+					}
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (isok) {
+				break;
+			}
+		}
+		// 从路径中获取
+		if (filename == null || "".equals(filename)) {
+			String url = conn.getURL().toString();
+			filename = url.substring(url.lastIndexOf("/") + 1);
+		}
+		return filename;
 	}
 
 	/**
@@ -146,9 +209,10 @@ public class UpdateAPKActivity extends Activity {
 				"application/vnd.android.package-archive");
 		startActivity(intent);
 	}
-	
+
 	/**
 	 * 在SD卡目录下创建文件夹
+	 * 
 	 * @param name
 	 * @return
 	 */
@@ -157,25 +221,30 @@ public class UpdateAPKActivity extends Activity {
 		String path = null;
 		if (Environment.getExternalStorageState().equals(
 				Environment.MEDIA_MOUNTED)) {
-			path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + name;
+			path = Environment.getExternalStorageDirectory().getAbsolutePath()
+					+ "/" + name;
 			File file = new File(path);
-			//创建文件夹
+			// 创建文件夹
 			file.mkdirs();
 			Log.v("Update", "CreateFileInSDCardSuccess");
+		} else {
+			Log.e("Update",
+					"SDCard State:" + Environment.getExternalStorageState());
 		}
 		return path;
 	}
-	
+
 	/**
 	 * 在data/data目录下创建文件夹
+	 * 
 	 * @param name
 	 * @return
 	 */
 	private String createFileInAppData(String name) {
 		String path = null;
-		path = this.getFilesDir() + "/" + name;
+		path = getApplicationContext().getFilesDir() + "/" + name;
 		File file = new File(path);
-		//创建文件夹
+		// 创建文件夹
 		Boolean isSuccess = file.mkdirs();
 		Log.v("Update", "CreateFileInDataSuccess" + isSuccess.toString());
 		return path;
@@ -236,11 +305,11 @@ public class UpdateAPKActivity extends Activity {
 		intentFilter.addDataScheme("package");
 		registerReceiver(apkInstallListener, intentFilter);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
-		//注销监听
+		// 注销监听
 		unregisterReceiver(apkInstallListener);
 		super.onDestroy();
 	}
